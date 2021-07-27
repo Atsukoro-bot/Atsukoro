a2c33d941a9764ebde83dd3a3c57af7ba6c8a490
Atsukoro1 <dornicakkuba@gmail.com>
2021-07-27 18:24:56 +0200

    Nsfw commands updated

---
+++ commands/nsfw/bj.js
@@ -0,0 +1,45 @@
+const axios = require("axios");
+let {
+    MessageEmbed
+} = require("discord.js");
+
+const {
+    bj
+} = require("../../data/apiLinks.json").nsfw.endpoints;
+const baseUrl = require("../../data/apiLinks.json").nsfw.baseUrl;
+
+module.exports = {
+    name: "bj",
+    description: "nsfw bj",
+    perms: [],
+    timeout: 3000,
+    category: "nsfw",
+    execute: async function (message, args, commands) {
+        const getData = async (url) => {
+            try {
+                const res = (await axios.get(url)).data;
+                return res.link;
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        const postEmbed = async (url, author) => {
+            try {
+                let embed = new MessageEmbed()
+                    .setColor("#5865F2")
+                    .setAuthor(author)
+                    .setImage(url);
+
+                return message.channel.send(embed);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        await postEmbed(
+            await getData(baseUrl + bj),
+            `Here is your bj! :3`
+        );
+    },
+};
---
+++ commands/nsfw/yuri.js
@@ -0,0 +1,45 @@
+const axios = require("axios");
+let {
+    MessageEmbed
+} = require("discord.js");
+
+const {
+    yuri
+} = require("../../data/apiLinks.json").nsfw.endpoints;
+const baseUrl = require("../../data/apiLinks.json").nsfw.baseUrl;
+
+module.exports = {
+    name: "yuri",
+    description: "nsfw yuri",
+    perms: [],
+    timeout: 3000,
+    category: "nsfw",
+    execute: async function (message, args, commands) {
+        const getData = async (url) => {
+            try {
+                const res = (await axios.get(url)).data;
+                return res.link;
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        const postEmbed = async (url, author) => {
+            try {
+                let embed = new MessageEmbed()
+                    .setColor("#5865F2")
+                    .setAuthor(author)
+                    .setImage(url);
+
+                return message.channel.send(embed);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        await postEmbed(
+            await getData(baseUrl + yuri),
+            `Here is your yuri! :3`
+        );
+    },
+};
