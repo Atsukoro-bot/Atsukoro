f7c1acc3a3de0ec6bbb48a99601ac561f69a2bd7
Matyáš Caras <contact@hernikplays.cz>
2021-08-01 20:05:20 +0200

    Functioning music quiz for more people

--- commands/animeandmanga/quiz.js
+++ commands/animeandmanga/quiz.js
@@ -6,15 +6,21 @@ var ss = require("string-similarity");
 const baseUrl = require("../../data/apiLinks.json").anime.baseUrl;
 
 let data = [];
-let wl = ["145973959127597057","724676555955241001","815210915510878228"]
+let wl = ["145973959127597057", "724676555955241001", "815210915510878228"];
 module.exports = {
   name: "quiz",
   description: "Get characters that has birthday today!",
   perms: [],
   timeout: 3000,
   category: "Anime & Manga",
-  execute: async function (message, args, commands) {
-    if(!wl.includes(message.author.id)) return message.channel.send("This command is still in testing and does not work.").then(m=>m.delete({timeout:5000}))
+  execute: async function (message, args, commands, bot) {
+    let rounds;
+    let currentRound = 1;
+    let points = {};
+    if (!wl.includes(message.author.id))
+      return message.channel
+        .send("This command is still in testing and does not work.")
+        .then((m) => m.delete({ timeout: 5000 }));
     if (args[0] == "set") {
       // command for setting anilist username
       if (!args[1])
@@ -30,6 +36,14 @@ module.exports = {
       });
       // end anilist username
     } else {
+      if (!args[0])
+        return message.channel.send(
+          "❗ Please specify the number of rounds (Currently capped at 10)"
+        ); // this could be more elegant
+      rounds = Math.round(parseInt(args[0]));
+      currentRound = 1;
+      if (rounds > 10) return;
+
       let db = await User.findOne({ id: message.author.id });
       if (!db)
         return message.channel.send(
@@ -37,9 +51,10 @@ module.exports = {
         );
       let user = db.anilist;
       if (!user) return;
-      
-      let vid = await getVid(user);
+
       let voiceChannel = message.member.voice.channel;
+      if (bot.voice.connections.get(message.guild.id))
+        return message.channel.send(":x: I am already in a voice channel");
       if (!voiceChannel)
         return message.channel.send(":x: You are not in a voice channel");
       if (!voiceChannel.joinable)
@@ -48,8 +63,56 @@ module.exports = {
         );
       voiceChannel.join().then((connection) => {
         // joins voice channel
+        message.channel.send("Quiz started in " + `<#${voiceChannel.id}>, write answers to <#${message.channel.id}>`);
+        points = {}
+        runRound(connection, user, voiceChannel.id,voiceChannel.members.map(m=>m.id));
+      });
+    }
+
+    /**
+     * Get vid link FROM https://themes.moe/api/
+     * @returns video object
+     */
+    async function getVid(user) {
+      let usr = (await axios.get("https://themes.moe/api/anilist/" + user))
+        .data;
+      //console.log(usr.length);
+      if (usr.length == 0) return {};
+      usr = usr.filter((e) => {
+        return e.watchStatus == 2 || e.watchStatus == 1;
+      });
+      let anime = usr[Math.floor(Math.random() * (usr.length - 1))];
+      let sorted = anime.themes.filter((e) => {
+        return e.themeType.includes("OP");
+      });
+      if(sorted.length == 0) return await getVid(user)
+      let random = sorted[Math.floor(Math.random() * (sorted.length - 1))];
+      return { name: anime.name, link: random.mirror.mirrorURL };
+    }
+
+    /**
+     * ran every round to initiate a new round
+     */
+    async function runRound(connection, user, id, allowedUsers) {
+      if (currentRound > rounds || !bot.voice.connections.get(message.guild.id)) {
+        connection.disconnect(); // leave on end
+        let finalem = new MessageEmbed()
+        let sort = []
+        let victory = ""
+        for (const id in points) {
+          sort.push([id,points[id]])
+        }
+        sort.sort((a,b)=>b-a)
+        sort.forEach(element => {
+          victory+=`<@${element[0]}> - ${element[1]} points\n`
+        });
+        finalem.setTitle("Game Over!")
+        .addField("Final Scores",victory)
+        message.channel.send(finalem)
+      } else {
+        let vid = await getVid(user);
         console.log(vid.name.toLowerCase());
-        var points = 0;
+        
         if (vid.link.split(".").pop() == "webm") {
           axios({
             method: "get",
@@ -61,44 +124,32 @@ module.exports = {
         } else {
           connection.play(vid.link); // plays in voice
         }
-        message.channel.send("Quiz started in " + `<#${voiceChannel.id}>`)
-        const filter = (e) => e.author.id == message.author.id;
+        const filter = (e) => allowedUsers.includes(e.author.id);
         let collector = message.channel.createMessageCollector(filter, {
           time: 20000,
         });
         collector.on("collect", (me) => {
-          var similarity = ss.compareTwoStrings(me.content.toLowerCase(), vid.name.toLowerCase());
+          var similarity = ss.compareTwoStrings(
+            me.content.toLowerCase(),
+            vid.name.toLowerCase()
+          );
+          console.log(points[me.author.id])
+          console.log(me.author.id)
+
           if (similarity > 0.8) {
-            me.react("✅")
+            me.react("✅");
+            me.delete()
+            if (!points[me.author.id]) points[me.author.id] = 1;
+            else points[me.author.id] += 1;
           }
         });
         collector.on("end", (me) => {
           message.channel.send("Correct answer: " + vid.name);
-          connection.disconnect(); // leave on end
+          currentRound++;
+          connection.play("")
+          runRound(connection, user, id, allowedUsers);
         });
-      });
+      }
     }
-
-   
-
-    /**
-     * Get vid link FROM https://themes.moe/api/
-     * @returns video object
-     */
-    async function getVid(user) {
-      let usr = (await axios.get("https://themes.moe/api/anilist/"+user)).data;
-      console.log(usr.length)
-      if (usr.length == 0) return {};
-      usr = usr.filter(e=>{
-        return e.watchStatus == 2 || e.watchStatus == 1;
-      })
-      let anime = usr[Math.floor(Math.random() * (usr.length - 1))];
-      let sorted = anime.themes.filter(e=>{
-        return e.themeType.includes("OP")
-      })
-      console.log(sorted)
-      let random = sorted[Math.floor(Math.random() * (sorted.length - 1))];
-      return {name:anime.name,link:random.mirror.mirrorURL}
-    }
-
-  }}
+  },
+};
